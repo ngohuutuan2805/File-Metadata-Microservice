7293c0702ac34ce67ba293d754f9b1400f49580e
ngohuutuan2805 <hey.tuan.vn@gmail.com>
2016-12-07 01:26:07 +0700

    Fixed bug when user do not select file yet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var multer = require('multer')
+var upload = multer()
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* POST Upload file with post request. */
+router.post('/get-file-size', upload.single('filename'), function (req, res, next) {
+
+  res.json({
+    "size":req.file.size
+  })
+
+})
 
 module.exports = router;
