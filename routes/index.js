a4426c7030e52c279fd07e68ead1829c4de98bfb
ngohuutuan2805 <hey.tuan.vn@gmail.com>
2016-12-14 13:12:32 +0700

    Config to Heroku

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,17 @@ var router = express.Router();
 var multer = require('multer')
 var upload = multer()
 
+console.log("hoo")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("hihi")
   res.render('index', { title: 'Express' });
 });
 
 /* POST Upload file with post request. */
 router.post('/get-file-size', upload.single('filename'), function (req, res, next) {
-
+  console.log("get file size")
   res.json({
     "size":req.file.size
   })
